a8069c5265eba20f69c2acc08a103c581a321f31
bhandarisiddharth256 <bhandarisiddharth256@gmail.com>
2026-04-29 11:54:54 +0530

    fixing bugs

--- frontend/src/api/expense.api.js
+++ frontend/src/api/expense.api.js
@@ -3,12 +3,12 @@ const API_URL = import.meta.env.VITE_API_URL;
 export const getExpenses = async (params = {}) => {
   const query = new URLSearchParams(params).toString();
 
-  const res = await fetch(`${API_URL}/api/expenses?${query}`);
+  const res = await fetch(`${API_URL}/expenses?${query}`);
   return res.json();
 };
 
 export const createExpense = async (data) => {
-  const res = await fetch(`${API_URL}/api/expenses`, {
+  const res = await fetch(`${API_URL}/expenses`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
