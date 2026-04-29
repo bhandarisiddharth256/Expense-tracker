40a2f50bd76978c450a19bab253d88067829ba07
bhandarisiddharth256 <bhandarisiddharth256@gmail.com>
2026-04-29 09:07:40 +0530

    setup postgres connection and create expenses schema with constraints

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pool from "./db/index.js";
 
 dotenv.config();
 
@@ -15,6 +16,16 @@ app.use(express.json());
 // test route
 app.get("/health", (req, res) => {
   res.json({ message: "Server is running 🚀" });
+});
+
+app.get("/db-test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "DB connection failed" });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
